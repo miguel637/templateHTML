4dc3412115f5e34f1a00a9cf4795412d2531774f
Miguel Gómez <40007235+miguel637@users.noreply.github.com>
2021-10-13 09:50:04 -0500

    botones pruebas

--- pruebas.js
+++ pruebas.js
@@ -17,3 +17,25 @@ var numeros = [];
     count = 0;
   }
 }   
+/*
+<script type="text/javascript">
+  var numeros = [];
+  function generateRadom(min, max){
+    for (var x= 0; x<100; x++){
+        numeros.push(Math.floor(Math.random()*5)+1);
+    }
+    console.log(numeros);
+    count = 0;
+    console.log("Histograma");
+    for (var i=1; i<=5; i++) {
+      for (var j=0; j<=numeros.length; j++) {
+        if(numeros[j]==i)
+        {
+          count++;
+        }
+      }
+      console.log(i+"." + "*".repeat(count));
+      count = 0;
+    }
+  }      
+  </script>*/
